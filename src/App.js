aa21487ca49ce32df35251661299a0b02a9b8bf1
Enkh-Amgalan <48120791+enkhmgln@users.noreply.github.com>
2024-03-04 23:14:40 +0800

    converting to typescript

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState ,useC} from 'react';
-import InputShortener from './components/InputShortener';
-import LinkResult from './components/LinkResult';
-import Cookies from 'js-cookie';
+import React, { useState } from "react";
+import InputShortener from "./components/InputShortener";
+import LinkResult from "./components/LinkResult";
 
 function App() {
-  const [InputValue,setInputValue] = useState('');
+  const [InputValue, setInputValue] = useState("");
   return (
-    <div className=" flex flex-col justify-center text-center items-center bg-white max-[768px]:h-[60vh] max-[768px]: ">
-      <InputShortener setInputValue={setInputValue}/>
- 
-      <LinkResult InputValue={InputValue}/>
+    <div className="flex flex-col justify-center text-center items-center bg-white max-[768px]:h-[60vh] ">
+      <InputShortener setInputValue={setInputValue} />
+
+      <LinkResult InputValue={InputValue} />
     </div>
   );
 }
---
+++ src/components/InputShortener/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Button from "../General/Button";
+
+type InputShortenerType = {
+  setInputValue: (value: string) => void;
+};
+
+const InputShortener = ({ setInputValue }: InputShortenerType) => {
+  const [value, setValue] = useState<string>("");
+  const handleClick = () => {
+    setInputValue(value);
+    setValue("");
+  };
+  return (
+    <div className="flex flex-col p-10 max-w-md   text-blue-400">
+      <h1 className="text-lg font-bold">
+        ХОЛБООС <span className="text-green-500">БОГИНОСГОГЧ</span>
+      </h1>
+      <input
+        type="text"
+        className="my-4 bg-gray-50 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5"
+        placeholder="Холбоос оруулна уу  "
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        required
+      />
+      <Button name="Богиносгох" onClick={handleClick} />
+    </div>
+  );
+};
+
+export default InputShortener;
